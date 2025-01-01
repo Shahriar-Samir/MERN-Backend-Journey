6d138508bf7e75af2c68d5a2aed3689bece5b45e
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 22:04:59 +0600

    Practices/Practice1 - add: routes

--- Practices/Practice1/src/app.ts
+++ Practices/Practice1/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { globalErrorHandler } from './middlewares/globalErrorHandler'
 import notFound from './middlewares/NotFound'
+import router from './app/routes/routes'
 const app = express()
 
 app.use(express.json())
@@ -8,6 +9,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/api',router)
 
 app.use(globalErrorHandler)
 app.use(notFound)
