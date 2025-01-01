de82acf224859c68821c09129c8231e316527289
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 19:22:39 +0600

    Practices/Practice1 - add: not found error handler middleware

--- Practices/Practice1/src/app.ts
+++ Practices/Practice1/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { globalErrorHandler } from './middlewares/globalErrorHandler'
+import notFound from './middlewares/NotFound'
 const app = express()
 
 app.use(express.json())
@@ -9,5 +10,6 @@ app.get('/', (req, res) => {
 })
 
 app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app
