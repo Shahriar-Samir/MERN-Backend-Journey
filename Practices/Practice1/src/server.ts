bdf36badf92df45385e0bee18dc4e3a3dfc99efc
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 18:32:24 +0600

    Practices/Practice1 project initialized

---
+++ Practices/Practice1/src/server.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import config from './app/config'
+import app from './app'
+
+async function main() {
+  await mongoose.connect(config.dbURI as string)
+  app.listen(config.port, () => {
+    console.log(`app listening on port ${config.port}`)
+  })
+}
+
+main()
