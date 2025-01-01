527b234178de378620f62ac61464ba22ce1eeebe
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 21:59:41 +0600

    Practices/Practice1 - update: users controller

--- Practices/Practice1/src/app/modules/users/users.controller.ts
+++ Practices/Practice1/src/app/modules/users/users.controller.ts
@@ -1,11 +1,17 @@
-import { sendResponse } from '../../../utils/sendResponse'
-import { createUserIntroDB } from './users.service'
+import httpStatus from 'http-status';
+import { sendResponse } from '../../../utils/sendResponse';
+import { createUserIntroDB } from './users.service';
 
 export const createUser = async (req, res, next) => {
   try {
-    const result = await createUserIntroDB(req.body)
-    return sendResponse(res, result)
+    const result = await createUserIntroDB(req.body);
+    return sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.ok as number,
+      message: 'User created successfully',
+      data: result,
+    });
   } catch (err) {
-    return next(err)
+    return next(err);
   }
-}
+};
