f994720807c6f09d15e1c422a6f480e5be3980a9
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 22:00:17 +0600

    Practices/Practice1 - update: sendRequest Utility

--- Practices/Practice1/src/utils/sendResponse.ts
+++ Practices/Practice1/src/utils/sendResponse.ts
@@ -1,16 +1,16 @@
-import { Response } from 'express'
+import { Response } from 'express';
 
 type TResponse<T> = {
-  success: boolean
-  message?: string
-  statusCode: number
-  data: T
-}
+  success: boolean;
+  message?: string;
+  statusCode: number;
+  data: T;
+};
 
 export const sendResponse = <T>(res: Response, data: TResponse<T>) => {
   return res.status(data.statusCode).json({
     success: data.success,
     message: data.message,
     statusCode: data.statusCode,
-  })
-}
+  });
+};
