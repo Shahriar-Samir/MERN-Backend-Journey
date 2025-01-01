8f11e89843536e176524058c4ed524ee102e5b82
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 18:54:55 +0600

    Practices/Practice1 - add: global error handler middleware

--- Practices/Practice1/src/app.ts
+++ Practices/Practice1/src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express'
+import { globalErrorHandler } from './middlewares/globalErrorHandler'
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(globalErrorHandler)
 
 export default app
